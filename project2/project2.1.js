f67009813b7109930b6607f9650a4e94fab04810
esromkidane99@gmail.com <esromkidane99@gmail.com>
2023-06-19 21:27:42 +0300

    add project2 to branch

--- project2/project2.1.js
+++
@@ -1,7 +0,0 @@
-function swapImage(clickedImage) {
-    var bigImage = document.querySelector(".big-image");
-    var bigImageSrc = bigImage.src;
-    bigImage.src = clickedImage.src;
-    clickedImage.src = bigImageSrc;
-  }
-  
